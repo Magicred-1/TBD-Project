b0b307c12e939286af8c76b2eabeaa4be6cd1d39
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-11-22 15:49:07 +0700

    Foundry Interroperability with Axelar and XRPL Main chain

--- TBD-main/packages/nextjs/components/SidebarComponent.tsx
+++ TBD-main/packages/nextjs/components/SidebarComponent.tsx
@@ -5,11 +5,13 @@ import { useRouter } from "next/navigation";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import { useWeb3AuthConnectorInstance } from "@/hooks/useWeb3AuthConnectorInstance";
 import { IconArrowLeft, IconBrandTabler, IconUserBolt } from "@tabler/icons-react";
+import { useXRPL } from "./web3auth/XRPLProvider/useXRPL";
 
 export function SidebarComponent() {
   const router = useRouter();
   const { web3AuthInstance } = useWeb3AuthConnectorInstance();
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { isConnected } = useXRPL();
 
   const isWeb3AuthConnected = web3AuthInstance && web3AuthInstance.status === "connected";
 
@@ -41,16 +43,22 @@ export function SidebarComponent() {
   return (
     <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} animate>
       <SidebarBody className="bg-gray-900 h-screen border-r border-gray-800 flex flex-col gap-6 py-6">
-        <div className="flex flex-col gap-2">
-          {links.map((link, index) => (
-            <SidebarLink
-              key={index}
-              link={link}
-              className="hover:bg-gray-800 transition-colors  mr-10 py-2 rounded-lg"
-            />
-          ))}
-        </div>
-      </SidebarBody>
+          <div className="flex flex-col gap-2">
+            {isConnected ? (
+              links.map((link, index) => (
+                <SidebarLink
+                key={index}
+                link={link}
+                className="hover:bg-gray-800 transition-colors  mr-10 py-2 rounded-lg"
+              />
+              ))
+            ) : (
+              <p className="text-wheat text-center">
+                Please connect first to access the dashboard
+              </p>
+            )}
+          </div>
+        </SidebarBody>
     </Sidebar>
   );
 }
